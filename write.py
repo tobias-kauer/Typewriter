d4c2ce84448e3fa62ed186840220bb0a4c55149b
Tobias Kauer <tobias.kauer@gmx.at>
2026-06-25 12:00:35 +0200

    update

--- write.py
+++ write.py
@@ -28,7 +28,7 @@ BRIDGE_TIME = 0.05
 INTER_KEY_DELAY = 0.05
 SETTLE_DELAY = 0.002
 # Time for row/column select lines to settle before enabling the mux bridge.
-MUX_CHANNEL_DELAY = 0.02
+MUX_CHANNEL_DELAY = 0.1
 IDLE_POLL_DELAY = 0.01
 
 KEYMAP = (
